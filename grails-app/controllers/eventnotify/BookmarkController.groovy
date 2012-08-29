cf7e95b287110b4327ff29dca62c41dae0904be1
kamatama41 <shiketaudonko41@gmail.com>
2012-08-29 09:36:02 +0900

    Atndのイベントサーチ処理追加

---
+++ grails-app/controllers/eventnotify/BookmarkController.groovy
@@ -0,0 +1,16 @@
+package eventnotify
+
+class BookmarkController {
+    def index() { 
+    }
+
+    def search() {
+        def events = atendApiService.search(params)
+        [eventList: events]
+    }
+
+    def show() {
+        def event = atendApiService.get(eventId)
+        [event: event]
+    }
+}
---
+++ test/unit/eventnotify/AtendServiceTests.groovy
@@ -0,0 +1,25 @@
+package eventnotify
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(AtendService)
+class AtendServiceTests {
+    @Test
+    void testSearch() {
+        def atendService = new AtendService()
+        def events = atendService.search(['keyword': 'TDD'])
+        events.each { event ->
+            println event.name
+            println event.date
+            println event.capacity
+            println event.place
+            println ''
+        }
+    }
+}
